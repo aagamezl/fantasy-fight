b1f29edcbf533f3c4a49472278bdb6d5669de2a3
Álvaro José Agámez Licha <alvaroagamez@outlook.com>
2022-11-03 00:12:54 +0100

    feat: initial commit

---
+++ js/config.js
@@ -0,0 +1,15 @@
+// Config values
+export const config = {
+  game: {
+    gravity: 1.4,
+    hitStrength: 5,
+    time: 100
+  },
+  canvas: {
+    height: 576,
+    width: 1024
+  },
+  attackBox: {
+    show: false
+  }
+}
